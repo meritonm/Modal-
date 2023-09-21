e4f608825e514364db9ca33ac189dc991ada96ba
Meriton <108494516+meritonm@users.noreply.github.com>
2023-09-21 15:15:23 +0200

    Add files via upload

---
+++ Modal/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+// console.log(btnsOpenModal);
+
+const openModal = function () {
+  // console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener(
+    "click",
+    openModal
+    //  {
+    // console.log("Button clicked");
+    // modal.classList.remove("hidden");
+    // overlay.classList.remove("hidden");
+    // }
+  );
+}
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
+
+btnCloseModal.addEventListener("click", closeModal);
+// {
+// modal.classList.add("hidden");
+// overlay.classList.add("hidden");
+// });
+
+overlay.addEventListener("click", closeModal);
+// {
+// modal.classList.add("hidden");
+// overlay.classList.add("hidden");
+// });
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hiden")) {
+    closeModal();
+  }
+});
